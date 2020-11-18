4ad737355ec229ae20d19e735099cb64f1e6d30f
vfischer9 <victoria.fischer@cix.csi.cuny.edu>
2020-11-17 20:08:56 -0500

    working branch

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.json("Test Profile");
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017';
+mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        return
+    }
+
+    app.get('/', function(req, res) {
+        res.json("Test Profile");
+    });
+
+    app.get('/profile/users', function(req, res) {
+        //req.app.get('locals.client').db('bettDb').collection('userAccounts').find().toArray((err, items) => {
+            res.json('hello');
+        //})
+    });
+
 });
+
 
 module.exports = app;
