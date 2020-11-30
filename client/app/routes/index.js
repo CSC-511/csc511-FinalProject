bd10a2b8a8ae4d74ae4313d979faeefc41a4fa4b
VincentPellechia <vinp13@msn.com>
2020-11-30 11:33:48 -0500

    Reverting Changes

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -5,23 +5,17 @@ import { inject as service } from '@ember/service';
 
 const FAKE_COOKIE = 123123123;
 export default class IndexRoute extends Route {
-  // constructor(){
-  //   super(...arguments)
-  //   this.model();
-  //   this.afterModel(this.model());
-  // }
-
-    model() {
-      const cookie = localStorage.getItem('cookie')
-      const temp = $.get(`${ENV.APP.API_ENDPOINT}/auth/isloggedin?cookie=${cookie}`);
-      console.log(temp)
-      return temp;
+    @service store;
+    
+    model(params) {
+      // const REAL_COOKIE = parseInt(params.cookie);
+      const cookie =localStorage.getItem('cookie');
+        return $.get(`${ENV.APP.API_ENDPOINT}/auth/isloggedin?cookie=${cookie}`);
     }
 
-    afterModel(model) {
-      if (!model) { // if there model is empty, take us to log in route
-        this.transitionTo('login');
+    afterModel(model, transition) {
+        if (!model) { // if there model is empty, take us to log in route
+          this.transitionTo('login');
+        }
       }
-      console.log(model);
-    }
 }
